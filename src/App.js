a6eaa2ae671d7b5eb389dab2a340f79b44443cc0
md-amir-gauhar <gauharamir15@gmail.com>
2021-07-28 22:46:00 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [prev, setPrev] = useState('');
+  const [current, setCurrent] = useState('');
+  const [quantity, setQuantity] = useState('');
+  const [text, setText] = useState('');
+
+  const clickHandler = () => {
+    if (prev <= 0 || quantity <= 0 || current <= 0) {
+      setText("Provided inputs should be grater than 0");
+    } else {
+      const then = Number(prev) * Number(quantity);
+      const now = Number(current) * Number(quantity);
+      let percent = (((now - then) / then) * 100).toFixed(2);
+
+      if (percent === 0) {
+        setText(`Percentage Gain: ${percent}% and Total Profit: ${now - then}`);
+      } else if (percent > 0) {
+        setText(
+          `Percentage Gain: ${percent}% and Total Profit: ${now - then} 🤑🥳`
+        );
+      } else {
+        setText(
+          `Percentage Loss: ${Math.abs(percent)}% and Total Loss: ₹${Math.abs(
+            now - then
+          )} 😞😞`
+        );
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header>
+        <h1>???Profit/Loss???</h1>
       </header>
+      <div className="input_container">
+        <div className="number flex">
+          <h4>Price per stock at the time of purchase: </h4>
+          <input type="number" value={prev} onChange={e => setPrev(e.target.value)} />
+        </div>
+        <div className="number flex">
+          <h4>Total number of stocks purchased: </h4>
+          <input type="number" value={quantity} onChange={e => setQuantity(e.target.value)} />
+        </div>
+        <div className="number flex">
+          <h4>Current price: </h4>
+          <input type="number" value={current} onChange={e => setCurrent(e.target.value)} />
+        </div>
+      </div>
+      <button onClick={clickHandler}>Check Status</button>
+      <p className="result">{text}</p>
     </div>
   );
 }
